9150cad29da3ff64ba8b52fe274364eaa3f84c94
Jack <jack.morgan.edwards@gmail.com>
2022-05-25 17:23:22 -0400

    added ability to add and delete items from cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { removeFromCart } from "../../store/cart";
+import { removeFromCart, incrementItem, decrementItem } from "../../store/cart";
 import { useDispatch } from "react-redux";
 
 function CartItem({ item }) {
@@ -10,15 +10,24 @@ function CartItem({ item }) {
   useEffect(() => {
     setCount(item.count);
   }, [item.count]);
-  console.log(item.id);
 
   return (
     <li className="cart-item">
       <div className="cart-item-header">{item.name}</div>
       <div className="cart-item-menu">
         <input type="number" value={count} />
-        <button className="cart-item-button">+</button>
-        <button className="cart-item-button">-</button>
+        <button
+          className="cart-item-button"
+          onClick={() => dispatch(incrementItem(item.id))}
+        >
+          +
+        </button>
+        <button
+          className="cart-item-button"
+          onClick={() => dispatch(decrementItem(item.id))}
+        >
+          -
+        </button>
         <button
           className="cart-item-button"
           onClick={() => dispatch(removeFromCart(item.id))}
--- src/store/cart.js
+++ src/store/cart.js
@@ -1,5 +1,7 @@
 const ADD_TO_CART = "cart/ADD";
 const REMOVE_FROM_CART = "cart/REMOVE";
+const INCREMENT_ITEM = "cart/INCREMENT";
+const DECREMENT_ITEM = "cart/DECREMENT";
 
 const cartReducer = (state = {}, action) => {
   switch (action.type) {
@@ -13,6 +15,22 @@ const cartReducer = (state = {}, action) => {
       newObj2 = { ...state };
       delete newObj2[action.id];
       return newObj2;
+    case INCREMENT_ITEM:
+      let newObj3 = {};
+      newObj3 = { ...state };
+      let prevCount = newObj3[action.id].count;
+      newObj3[action.id] = { id: action.id, count: prevCount + 1 };
+      return newObj3;
+    case DECREMENT_ITEM:
+      let newObj4 = {};
+      newObj4 = { ...state };
+      let prevCount2 = newObj4[action.id].count - 1;
+      if (prevCount2 <= 0) {
+        delete newObj4[action.id];
+      } else {
+        newObj4[action.id] = { id: action.id, count: prevCount2 - 1 };
+      }
+      return newObj4;
     default:
       return state;
   }
@@ -28,6 +46,20 @@ export const addToCart = (id) => {
 export const removeFromCart = (id) => {
   return {
     type: REMOVE_FROM_CART,
+    id,
+  };
+};
+
+export const incrementItem = (id) => {
+  return {
+    type: INCREMENT_ITEM,
+    id,
+  };
+};
+
+export const decrementItem = (id) => {
+  return {
+    type: DECREMENT_ITEM,
     id,
   };
 };
